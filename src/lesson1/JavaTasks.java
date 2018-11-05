4391d05375d055e2b1bc782ccea7b27b418b645c
CrazyJen <Jaller@inbox.ru>
2018-11-05 16:09:06 +0300

    Lessons 1 & 2

--- src/lesson1/JavaTasks.java
+++ src/lesson1/JavaTasks.java
@@ -2,64 +2,105 @@ package lesson1;
 
 import kotlin.NotImplementedError;
 
+import java.io.*;
+import java.util.ArrayList;
+
 @SuppressWarnings("unused")
 public class JavaTasks {
     /**
      * Сортировка времён
-     *
+     * <p>
      * Простая
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся моменты времени в формате ЧЧ:ММ:СС,
      * каждый на отдельной строке. Пример:
-     *
+     * <p>
      * 13:15:19
      * 07:26:57
      * 10:00:03
      * 19:56:14
      * 13:15:19
      * 00:40:31
-     *
+     * <p>
      * Отсортировать моменты времени по возрастанию и вывести их в выходной файл с именем outputName,
      * сохраняя формат ЧЧ:ММ:СС. Одинаковые моменты времени выводить друг за другом. Пример:
-     *
+     * <p>
      * 00:40:31
      * 07:26:57
      * 10:00:03
      * 13:15:19
      * 13:15:19
      * 19:56:14
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
     static public void sortTimes(String inputName, String outputName) {
-        throw new NotImplementedError();
+        ArrayList<Integer> inputList = new ArrayList<>();
+        try {
+            String readTime;
+            File file = new File(inputName);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            while ((readTime = reader.readLine()) != null) {
+                String[] parsedTime = readTime.split(":");
+                inputList.add(Integer.parseInt(parsedTime[0]) * 3600 + Integer.parseInt(parsedTime[1]) * 60 +
+                        Integer.parseInt(parsedTime[2]));
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        int[] times = new int[inputList.size()];
+        times = inputList.stream().mapToInt(i -> i).toArray();
+
+        Sorts.quickSort(times);
+
+
+        StringBuilder output = new StringBuilder();
+
+        for (int time : times) {
+            output.append(String.format("%02d:%02d:%02d\n", time / 3600,
+                    (time / 60) % 60, time % 60));
+        }
+
+
+        try {
+            FileWriter out = new FileWriter(outputName);
+            out.write(output.toString());
+            out.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 
     /**
      * Сортировка адресов
-     *
+     * <p>
      * Средняя
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся фамилии и имена жителей города с указанием улицы и номера дома,
      * где они прописаны. Пример:
-     *
+     * <p>
      * Петров Иван - Железнодорожная 3
      * Сидоров Петр - Садовая 5
      * Иванов Алексей - Железнодорожная 7
      * Сидорова Мария - Садовая 5
      * Иванов Михаил - Железнодорожная 7
-     *
+     * <p>
      * Людей в городе может быть до миллиона.
-     *
+     * <p>
      * Вывести записи в выходной файл outputName,
      * упорядоченными по названию улицы (по алфавиту) и номеру дома (по возрастанию).
      * Людей, живущих в одном доме, выводить через запятую по алфавиту (вначале по фамилии, потом по имени). Пример:
-     *
+     * <p>
      * Железнодорожная 3 - Петров Иван
      * Железнодорожная 7 - Иванов Алексей, Иванов Михаил
      * Садовая 5 - Сидоров Петр, Сидорова Мария
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
     static public void sortAddresses(String inputName, String outputName) {
@@ -68,14 +109,14 @@ public class JavaTasks {
 
     /**
      * Сортировка температур
-     *
+     * <p>
      * Средняя
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле заданы температуры различных участков абстрактной планеты с точностью до десятых градуса.
      * Температуры могут изменяться в диапазоне от -273.0 до +500.0.
      * Например:
-     *
+     * <p>
      * 24.7
      * -12.6
      * 121.3
@@ -83,11 +124,11 @@ public class JavaTasks {
      * 99.5
      * -12.6
      * 11.0
-     *
+     * <p>
      * Количество строк в файле может достигать ста миллионов.
      * Вывести строки в выходной файл, отсортировав их по возрастанию температуры.
      * Повторяющиеся строки сохранить. Например:
-     *
+     * <p>
      * -98.4
      * -12.6
      * -12.6
@@ -102,12 +143,12 @@ public class JavaTasks {
 
     /**
      * Сортировка последовательности
-     *
+     * <p>
      * Средняя
      * (Задача взята с сайта acmp.ru)
-     *
+     * <p>
      * В файле задана последовательность из n целых положительных чисел, каждое в своей строке, например:
-     *
+     * <p>
      * 1
      * 2
      * 3
@@ -115,12 +156,12 @@ public class JavaTasks {
      * 3
      * 1
      * 2
-     *
+     * <p>
      * Необходимо найти число, которое встречается в этой последовательности наибольшее количество раз,
      * а если таких чисел несколько, то найти минимальное из них,
      * и после этого переместить все такие числа в конец заданной последовательности.
      * Порядок расположения остальных чисел должен остаться без изменения.
-     *
+     * <p>
      * 1
      * 3
      * 3
@@ -135,19 +176,40 @@ public class JavaTasks {
 
     /**
      * Соединить два отсортированных массива в один
-     *
+     * <p>
      * Простая
-     *
+     * <p>
      * Задан отсортированный массив first и второй массив second,
      * первые first.size ячеек которого содержат null, а остальные ячейки также отсортированы.
      * Соединить оба массива в массиве second так, чтобы он оказался отсортирован. Пример:
-     *
+     * <p>
      * first = [4 9 15 20 28]
      * second = [null null null null null 1 3 9 13 18 23]
-     *
+     * <p>
      * Результат: second = [1 3 4 9 9 13 15 20 23 28]
      */
     static <T extends Comparable<T>> void mergeArrays(T[] first, T[] second) {
-        throw new NotImplementedError();
+        int i = 0;
+        int j = first.length;
+        int k = 0;
+        while (i < first.length && j < second.length) {
+            if (first[i].compareTo(second[j]) <= 0) {
+                second[k] = first[i];
+                k++;i++;
+            } else {
+                second[k] = second[j];
+                k++; j++;
+            }
+        }
+
+        while (i < first.length) {
+            second[k] = first[i];
+            k++;i++;
+        }
+        while (j < second.length) {
+            second[k] = second[j];
+            k++; j++;
+        }
     }
 }
+
